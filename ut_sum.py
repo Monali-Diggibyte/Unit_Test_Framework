3b882aec91a3e16d19a5955057eff85978bb9f24
Monali-Diggibyte <108858660+Monali-Diggibyte@users.noreply.github.com>
2022-07-08 17:48:23 +0530

    Add files via upload

---
+++ ut_sum.py
@@ -0,0 +1,23 @@
+import unittest
+    
+class SumTest(unittest.TestCase):
+    
+    def sum(input1, input2):
+        return input1+input2
+        
+    def sum_testfunc_1(self):
+        # Arrange- create variable
+        input1 = 10
+        input2 = 20
+        expected_output = 30
+
+        # Act - call the function
+        result = sum(input1, input2)
+        print("Result:",result)
+            
+        # Assert- assert Result
+        
+        self.assertEqual(result, expected_output)
+    
+if __name__ == '__main__':
+    unittest.main()
