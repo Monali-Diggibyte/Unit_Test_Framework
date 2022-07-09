0a5e5721a58193f62a7f8eba4000e14f6aa7a28f
Monali-Diggibyte <108858660+Monali-Diggibyte@users.noreply.github.com>
2022-07-09 09:51:35 +0530

    Add files via upload

--- ut_sum.py
+++ ut_sum.py
@@ -1,20 +1,18 @@
 import unittest
-    
+
 class SumTest(unittest.TestCase):
     
-    def sum(input1, input2):
-        return input1 + input2
-        
-    def sum_testfunc_1(self):
+    def test_func1(self):
         # Arrange- create variable
         input1 = 10
         input2 = 20
-        expected_output = 30
+        expected_output = 40
         # Act - call the function
-        result = sum(input1, input2)
+        # result = sum(input1, input2)
+        result = input1 + input2
         print("Result:",result)
         # Assert- assert Result  
-        self.assertEqual(result, expected_output, msg="result and expected output are same")
-    
+        self.assertEqual(result, expected_output, msg="result and expected output are NOT same")
+
 if __name__ == '__main__':
     unittest.main()
